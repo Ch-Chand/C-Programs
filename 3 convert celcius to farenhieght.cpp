c00c872ec597da6d71805051771b91a71f4e0dba
Chand <meetchchand@gmail.com>
2021-02-05 00:08:53 +0500

    celcius to farenheight

---
+++ 3 convert celcius to farenhieght.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int c;
+	float f;
+	cout<<"Enter temperature in celcius :";
+	cin>>c;
+	f=(9/5)*c+32;
+	cout<<"Equalent in farenhieght="<<f;
+	return 0;
+}
