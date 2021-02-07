156d05dcfcbe3c822cb38225809e895abfe91c16
Chand <meetchchand@gmail.com>
2021-02-07 21:38:18 +0500

    ASCII value of a character

---
+++ 5 ASCII value of a character.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter a character :";
+	 cin>>ch;
+	 cout<<"ASCII equalent value is :"<<int(ch);
+	return 0;
+}
