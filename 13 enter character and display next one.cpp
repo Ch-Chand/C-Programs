19a7870ed5c0423355f65ca5a2d38a1f75bace63
Chand <meetchchand@gmail.com>
2021-04-12 07:32:55 +0500

    enter character and display next one

---
+++ 13 enter character and display next one.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter a character :";
+	 cin>>ch;
+	 cout<<"The next character is :"<<++ch;
+	return 0;
+}
